7c09e0a569284ee1bd46d56e07d2a77cd0232c8c
antoineLAU <xab97300@gmail.com>
2026-09-09 01:24:12 +0200

    frontend: Acceuil connecte au backend avec message

--- frontend/src/page/acceuil/Acceuil.jsx
+++ frontend/src/page/acceuil/Acceuil.jsx
@@ -1,14 +1,24 @@
-import Bouton from '../../conponent/bouton/Bouton';
-import './Acceuil.css'
+import { useState, useEffect } from "react";
+import Bouton from "../../conponent/bouton/Bouton";
+import "./Acceuil.css";
 
 function Acceuil() {
+  const [message, setMessage] = useState("connexion en cours");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
 
   return (
     <>
-    <Bouton texte="Connexion" />
-    <Bouton texte="Inscription" />
+      <h1>Imaginarium</h1>
+      <p>{message}</p>
+      <Bouton texte="Connexion" />
+      <Bouton texte="Inscription" />
     </>
-  )
+  );
 }
 
 export default Acceuil;
