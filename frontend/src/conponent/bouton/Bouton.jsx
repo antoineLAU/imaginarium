73e87082532223fb8fba7691c4b6efd4b1022161
antoineLAU <xab97300@gmail.com>
2026-09-09 01:10:05 +0200

    frontend: ajout composant Bouton avec prop texte + page Acceuil

---
+++ frontend/src/conponent/bouton/Bouton.jsx
@@ -0,0 +1,7 @@
+function Bouton({ texte }) {
+  return (
+    <button>{texte}</button>
+  );
+}
+
+export default Bouton;
---
+++ frontend/src/page/acceuil/Acceuil.jsx
@@ -0,0 +1,14 @@
+import Bouton from '../../conponent/bouton/Bouton';
+import './Acceuil.css'
+
+function Acceuil() {
+
+  return (
+    <>
+    <Bouton texte="Connexion" />
+    <Bouton texte="Inscription" />
+    </>
+  )
+}
+
+export default Acceuil;
